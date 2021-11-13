b6ed013ab8064c051c74ac793c84003df1f28085
olgachotii <72124804+olgachotii@users.noreply.github.com>
2021-11-13 13:52:36 +0200

    2

--- src/index.js
+++ src/index.js
@@ -1,22 +1,45 @@
 import './sass/main.scss';
-import gatePage from './js/apiService.js';
+import NewApiService from './js/apiService.js';
 import cards from './templates/cards.hbs';
 
 const refs = {
   gallery: document.querySelector('.gallery'),
-  input: document.querySelector('.input'),
-  formButton: document.querySelector('.form__button'),
-};
 
-refs.formButton.addEventListener('click', onClickForm);
+  searchForm: document.querySelector('.search-form'),
+  loadMoreBtn: document.querySelector('.load-more'),
+};
+const newApiService = new NewApiService();
+
+refs.searchForm.addEventListener('submit', onClickForm);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
 function onClickForm(e) {
   e.preventDefault();
-  const whatToSearch = refs.input.value;
 
-  gatePage(whatToSearch).then(renderGalery);
+  clearGallery();
+  newApiService.query = e.currentTarget.elements.query.value;
+  newApiService.resetPage();
+  newApiService.gatePage().then(renderGalery);
+  refs.loadMoreBtn.classList.remove('is-hidden');
+}
+
+function onLoadMore() {
+  console.log('~  newApiService.pageNumber', newApiService.pageNumber);
+  newApiService.gatePage().then(renderGalery);
 }
 
 function renderGalery(card) {
-  return (refs.gallery.innerHTML = cards(card.hits));
+  refs.gallery.insertAdjacentHTML('beforeend', cards(card));
+  scrollInto();
+}
+
+function clearGallery() {
+  refs.gallery.innerHTML = '';
+}
+
+function scrollInto() {
+  refs.loadMoreBtn.scrollIntoView({
+    behavior: 'smooth',
+    block: 'end',
+  });
 }
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,8 +1,29 @@
-const CAY_API = '24287584-f260c6215a8f38269d114f00b';
+export default class NewApiService {
+  constructor() {
+    this.CAY_API = '24287584-f260c6215a8f38269d114f00b';
+    this.pageNumber = 1;
+    this.whatToSearch = '';
+  }
 
-export default function gatePage(whatToSearch, pageNumber = 1) {
-  return fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${whatToSearch}&page=${pageNumber}&per_page=12&key=${CAY_API}
-`).then(res => {
-    return res.json();
-  });
+  gatePage() {
+    return fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.whatToSearch}&page=${this.pageNumber}&per_page=12&key=${this.CAY_API}
+`)
+      .then(res => res.json())
+      .then(({ hits }) => {
+        this.pageNumber += 1;
+        return hits;
+      });
+  }
+
+  resetPage() {
+    this.pageNumber = 1;
+  }
+
+  get query() {
+    return this.whatToSearch;
+  }
+
+  set query(newQuery) {
+    this.whatToSearch = newQuery;
+  }
 }
